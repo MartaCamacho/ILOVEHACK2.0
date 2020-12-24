7fd35b901ddd80cf6acd6fea07d164071be9917b
martacamacho <martacamachovargas@gmail.com>
2020-12-25 00:46:31 +0100

    working on the answers to connect to the database

--- routes/auth.js
+++ routes/auth.js
@@ -37,22 +37,14 @@ router.post(
           birthdate,
           gender,
           email,
+          answers,
           description,
-          /* answers: [question1,
-             question2,
-             question3,
-             question4,
-             question5,
-             question6,
-             question7,
-             question8,
-             question9,
-             question10], */
-             isHorny, 
-             searchFor,
+          isHorny, 
+          searchFor,
           
           // imgPath,
          });
+         console.log(newUser, 'el user')
         req.session.currentUser = newUser;
         res
           .status(200) 
