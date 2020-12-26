aff25d8ff008d29f3dc27fffe67d7afabba730c6
Eric Blair <erbmart7@gmail.com>
2020-12-26 21:13:56 +0100

    events editable

--- routes/events.js
+++ routes/events.js
@@ -43,7 +43,6 @@ router.post(
         await Event.findByIdAndUpdate(newEvent._id, {$addToSet:{attending: req.session.currentUser}})
       }
 
-      // console.log(newEvent)
       res.status(200).json(newEvent)
     } catch (error) {
       next(error);
@@ -68,67 +67,40 @@ router.get("/all-events", async (req, res, next) => {
 //EVENT DETAILS
 
 router.get("/event-details/:id", async (req, res, next) => {
-  const { id } = req.params;
-  console.log(id);
-  const theEvent = await Event.findById(id)
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const theEvent = await Event.findById(id).populate('attending')
     res.json( theEvent );
-  console.log(theEvent)
+  } catch (error) {
+    console.log(error)
+  }
+  
+ 
 });
 
 //EDIT EVENT INFORMATION
 
-router.put(
-  "/edit",
-  (req, res, next) => {
-    const { fullname, description, date, location, isPublic, cohort, imgPath } = req.body;
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, "0");
-    var mm = String(today.getMonth() + 1).padStart(2, "0");
-    var yyyy = today.getFullYear();
-
-    today = mm + dd + yyyy;
-    if (date < today) {
-      res.render("events/add-event", {
-        errorMessage: "The event has to happen in the future :)",
-      });
-      return;
-    } else if (fullname.length < 5) {
-      res.render("events/add-event", {
-        errorMessage: "Your event name should have at least 5 characters",
-      });
-      return;
-    } else if (description.length < 5) {
-      res.render("events/add-event", {
-        errorMessage: "Write a longer description!",
-      });
-      return;
-    } else if (location.length < 3) {
-      res.render("events/add-event", {
-        errorMessage:
-          "People will need to know where to go! Tell them the place ^^",
-      });
-      return;
-    }
-
-    Event.findByIdAndUpdate(
-      { _id: req.query.event_id },
-      { $set: { name, description, date, location, isPublic, cohort, imgPath } }
-    )
-      .then((event) => { res.json(event) })
-      .catch((error) => {
-        console.log(error);
-      });
+router.put("/edit/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { name, imgPath, description, date, time, location, isAttending, cohort, isPublic } = req.body;
+    const eventEdited = await Event.findByIdAndUpdate(id, {$set: {name, imgPath, description, date, time, location, isAttending, cohort, isPublic }}) 
+    res.status(200).json(eventEdited)
+  } catch (error) {
+    console.log(error)
   }
-);
+    
+  });
 
-//EDIT EVENT PICTURE
-// router.post("/upload", uploadCloud.single("imgPath"), (req, res, next) => {
-//   if (!req.file) {
-//     next(new Error("No file uploaded!"));
-//     return;
-//   }
-//   res.json({ secure_url: req.file.secure_url });
-// });
+// EDIT EVENT PICTURE
+router.post("/upload", uploadCloud.single("imgPath"), (req, res, next) => {
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  res.json({ secure_url: req.file.secure_url });
+});
 
 //DELETE EVENT
 
